254cced13e86a087c44bd7d4f4bbc94bd55ec57f
Dayana0410 <dayanasa93@gmail.com>
2023-12-11 22:12:24 -0500

    Menu y boton Pause

---
+++ RacingGame/Assets/Scripts/SceneResetter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneResetter : MonoBehaviour
+{
+    public void ResetScene()
+    {
+        // Obtiene el índice de la escena actual y la recarga
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
+
